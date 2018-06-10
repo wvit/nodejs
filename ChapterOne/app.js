597c6563ed637b27f728d26098fbe16b116173c6
wvit <1999wuwei@gmail.com>
2018-06-10 22:01:52 +0800

    express和jade的简单使用

---
+++ ChapterOne/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const port = process.env.PORT || 1999;
+const app = express();
+
+//设置根路径
+app.set('views', './views');
+//设置模板引擎
+app.set('view engine', 'jade');
+app.listen(port);
+
+console.log('start succeed ' + port);
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: '首页'
+  })
+  //发送html文件
+  // res.sendFile(__dirname + '/views/index.html');
+})
+
+app.get('/movis/:id', (req, res) => {
+  res.render('detail', {
+    title: '详情页'
+  })
+
+})
+
+app.get('/admin/movie', (req, res) => {
+  res.render('admin', {
+    title: '管理页'
+  })
+
+})
+
+app.get('/admin/list', (req, res) => {
+  res.render('list', {
+    title: '列表页'
+  })
+
+})
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nodejs
 nodejs入门学习
+
+ChapterOne
+第一阶段
+
