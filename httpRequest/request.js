8e4c78e99bbae9d45a4b7833723e3132fd23653f
wvit <1999wuwei@gmail.com>
2019-01-12 17:04:06 +0800

    发布到npm

---
+++ httpRequest/request.js
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+const inquirer = require('inquirer');
+const axios = require('axios');
+const program = require('commander');
+
+program
+    .version('0.0.1')
+    .parse(process.argv)
+
+const options = [{
+        type: "input",
+        name: "method",
+        message: "request type(请求类型) : "
+    },
+    {
+        type: "input",
+        name: "url",
+        message: "request url(请求地址) : "
+    },
+    {
+        type: "input",
+        name: "data",
+        message: "request arguments(请求参数) : "
+    }
+]
+
+inquirer.prompt(options).then(result => {
+    console.log(result)
+    if (result.method.toUpperCase() === 'GET') {
+        let data = '';
+        for (let key in data) {
+            reqData += `${key}=${data[key]}&`
+        }
+        return axios.get(`${result.url}?${data.slice(0, -1)}`);
+    } else if (result.method.toUpperCase() === 'POST') {
+        return axios.post(result.url, result.data);
+    }
+}).then(res => {
+    console.log(res.data)
+})
--- terminalInput/app.js
+++
@@ -1,8 +0,0 @@
-const program = require('commander')
-program
-    .version('0.0.1')
-    .option('-l, --list [list]')
-    .parse(process.argv)
-
-console.log(program.list)
-console.log(process.argv)
